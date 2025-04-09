2b97f0e3c3a164fe61855bccb544714d964007c8
Pedro-Moreno <pedromorenoc2000@hotmail.com>
2025-04-08 22:23:55 -0300

    Automação do slider (JS) + adição de novas imagens

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function() {
+    nextImage();
+}, 5000)
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
+
 function alterarQuantidade(valor) {
     let spanQuantidade = document.getElementById("quantidade");
     let quantidade = parseInt(spanQuantidade.innerText);
